b90131d1d73478a85bf1cb883ee64801408bdc15
heonjunkim <s01077314486@gmail.com>
2025-03-24 19:00:00 +0900

    2d

---
+++ 2738.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h> //malloc
+
+int main()
+{		
+	int a, b;
+	int arr[101][101] ;
+	int arr2[101][101];
+	scanf("%d %d", &a, &b);
+	//arr = (int*)malloc(sizeof(int) * a);
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int k = 0; k < b; k++)
+		{
+			scanf("%d", &arr[i][k]);
+		}
+	}
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int k = 0; k < b; k++)
+		{
+			scanf("%d", &arr2[i][k]);
+		}
+	}
+	for (int i = 0; i < a; i++)
+	{
+		for (int k = 0; k < b; k++) printf("%d ", arr[i][k] + arr2[i][k]);
+					
+		printf("\n");
+	}
+
+	return 0;
+}
