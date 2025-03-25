78085983a01b35d66f2d4b9d243025c1d159638d
heonjunkim <s01077314486@gmail.com>
2025-03-25 11:04:07 +0900

    2D_maxvalue_position

---
+++ 2566.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int line[9][9]={0};
+//	int max=0;
+//	int v = 0, h = 0;
+//	for (int i = 0; i < 9; i++)
+//	{
+//		for (int j = 0; j < 9; j++)
+//		{		
+//			scanf("%d", &line[i][j]);
+//			if (max < line[i][j]){
+//				max = line[i][j];					
+//				v = i;
+//				h = j;
+//			}
+//		}
+//	}
+//	printf("%d\n",max);
+//	printf("%d %d", v+1, h+1);
+//
+//	return 0;
+//}
