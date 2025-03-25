6c3bc36e89cd17f5884926b67442e69c7d9197fb
heonjunkim <s01077314486@gmail.com>
2025-03-25 18:30:27 +0900

    moji2

--- 10798.c
+++ 10798.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main()
-{
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char c1[5][15] = { 0 };
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		scanf("%s", c1[i], sizeof(c1));
+//	}
+//
+//	for (int i = 0; i < 15; i++)
+//	{
+//		for (int k = 0; k < 5; k++)
+//		{
+//			if (c1[k][i] == NULL){
+//				continue;
+//			}
+//			else{
+//				printf("%c", c1[k][i]);
+//			}
+//			
+//		}		
+//	}
+//	return 0;
+//}
